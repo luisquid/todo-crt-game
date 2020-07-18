27a0cc8c55958ee4bb573dd8529ea2873bf28ec5
Sofia Brown <sofia.brown@unity3d.com>
2020-07-18 15:46:09 -0500

    Environment
    Added enviroment package
    Redid lighting
    Added SceneController to load scene.

---
+++ Assets/_todogj/Scripts/SceneController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private readonly string environmentSceneIndex = "Environment";
+
+    public bool loadTest = false;
+
+    #region TEST
+    private void Start()
+    {
+        if (loadTest)
+        {
+            StartCoroutine(IE_LoadTest());
+        }
+    }
+
+    private IEnumerator IE_LoadTest()
+    {
+        yield return new WaitForSeconds(15);
+        LoadEnvironment();
+    }
+    
+    #endregion
+   
+
+    public void LoadEnvironment()
+    {
+        StartCoroutine(IE_LoadAdditive(environmentSceneIndex));
+    }
+    
+    private IEnumerator IE_LoadAdditive(string sceneIndex)
+    {
+        AsyncOperation asyncOp = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
+
+        while (!asyncOp.isDone)
+        {
+            yield return null;
+        }
+        
+        SceneManager.SetActiveScene( SceneManager.GetSceneByName( environmentSceneIndex ) );
+        Debug.Log("Scene loaded!");
+        
+    }
+}
