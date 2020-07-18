9b7f1268658ceae6478cbb0235c345a007879490
Squid <luisbazan417@gmail.com>
2020-07-18 03:25:13 -0500

    Project Upload

---
+++ Assets/_todogj/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed;
+
+    Rigidbody2D rb;
+    Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        anim.SetInteger("Walk", (int)(Mathf.Abs(h) + Mathf.Abs(v)));
+
+        rb.MovePosition((Vector2)transform.position + new Vector2(h, v) * playerSpeed * Time.deltaTime);
+    }
+}
