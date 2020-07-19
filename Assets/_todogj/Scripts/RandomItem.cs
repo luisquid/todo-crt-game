93e448a0273a2c49470d91afd27b9f2c4128eaf2
Squid <luisbazan417@gmail.com>
2020-07-19 15:51:51 -0500

    Project Version 1

---
+++ Assets/_todogj/Scripts/RandomItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomItem : MonoBehaviour
+{
+    public GameObject item;
+    public GameObject enemy;
+    public float randomSpeed = 0.5f;
+
+    private void OnEnable()
+    {
+        item.SetActive(false);
+        enemy.SetActive(true);
+
+        StartCoroutine(ChangeItem());
+    }
+
+    IEnumerator ChangeItem()
+    {
+        item.SetActive(!item.activeSelf);
+        enemy.SetActive(!item.activeSelf);
+
+        yield return new WaitForSeconds(randomSpeed);
+
+        StartCoroutine(ChangeItem());
+    }
+}
